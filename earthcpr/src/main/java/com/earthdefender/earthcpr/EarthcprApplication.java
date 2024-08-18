a6b438b02912f374dad8ee4cb7f22099cb687fde
haazz <gkwldnjs121@gmail.com>
2024-08-18 23:31:20 +0900

    init: spring boot

---
+++ earthcpr/src/main/java/com/earthdefender/earthcpr/EarthcprApplication.java
@@ -0,0 +1,13 @@
+package com.earthdefender.earthcpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthcprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthcprApplication.class, args);
+	}
+
+}
