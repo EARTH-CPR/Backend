25a6546961afff39f0cdac4104e48c321c79ee62
haazz <gkwldnjs121@gmail.com>
2024-08-21 02:31:27 +0900

    feat: shinhan API 연결
    webclient를 활용한 API 연결 구현
    Generic한 ApiService 구현

---
+++ earthcpr/src/main/java/com/earthdefender/earthcpr/DTO/ShinhanApiDTO.java
@@ -0,0 +1,83 @@
+package com.earthdefender.earthcpr.DTO;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class ShinhanApiDTO {
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class RequestHeader {
+        @JsonProperty("Header")
+        private RequestHeaderParam header;
+    }
+
+    @Builder
+    @Data
+    public static class RequestHeaderParam {
+        @JsonProperty("apiName")
+        private String apiName;
+
+        @JsonProperty("transmissionDate")
+        private String transmissionDate;
+
+        @JsonProperty("transmissionTime")
+        private String transmissionTime;
+
+        @JsonProperty("institutionCode")
+        private String institutionCode;
+
+        @JsonProperty("fintechAppNo")
+        private String fintechAppNo;
+
+        @JsonProperty("apiServiceCode")
+        private String apiServiceCode;
+
+        @JsonProperty("institutionTransactionUniqueNo")
+        private String institutionTransactionUniqueNo;
+
+        @JsonProperty("apiKey")
+        private String apiKey;
+
+        @JsonProperty("userKey")
+        private String userKey;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ResponseHeader {
+        @JsonProperty("responseCode")
+        private String responseCode;
+
+        @JsonProperty("responseMessage")
+        private String responseMessage;
+
+        @JsonProperty("apiName")
+        private String apiName;
+
+        @JsonProperty("transmissionDate")
+        private String transmissionDate;
+
+        @JsonProperty("transmissionTime")
+        private String transmissionTime;
+
+        @JsonProperty("institutionCode")
+        private String institutionCode;
+
+        @JsonProperty("apiKey")
+        private String apiKey;
+
+        @JsonProperty("apiServiceCode")
+        private String apiServiceCode;
+
+        @JsonProperty("institutionTransactionUniqueNo")
+        private String institutionTransactionUniqueNo;
+    }
+}
--- earthcpr/src/main/java/com/earthdefender/earthcpr/response/ErrorCode.java
+++ earthcpr/src/main/java/com/earthdefender/earthcpr/response/ErrorCode.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "request 필수 항목을 확인하세요."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error."),
     ;
 
     private final HttpStatus httpStatus;
