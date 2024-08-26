a3188ebe59dcaf34e4ad89d21fb0c6872b32e12a
haazz <gkwldnjs121@gmail.com>
2024-08-26 17:45:43 +0900

    fix: saving products

--- earthcpr/src/main/java/com/earthdefender/earthcpr/controller/SaveController.java
+++ earthcpr/src/main/java/com/earthdefender/earthcpr/controller/SaveController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/save")
 @RequiredArgsConstructor
@@ -19,11 +21,13 @@ public class SaveController {
 
     @PostMapping("/create/savingproduct")
     public ResponseEntity<ApiResponseEntity> createSavingProduct(@Valid @RequestBody SavingProductDTO.ProductData productData) {
-        return saveService.createSavingsProduct(productData);
+        saveService.createSavingsProduct(productData);
+        return ApiResponseEntity.toResponseEntity();
     }
 
     @PostMapping("/get/savingproducts")
     public ResponseEntity<ApiResponseEntity> getSavingProductList() {
-        return ApiResponseEntity.toResponseEntity(saveService.getSavingProductList());
+        List<SavingProductDTO.ProductData> savingProductsResponseList =  saveService.getSavingProductList();
+        return ApiResponseEntity.toResponseEntity(savingProductsResponseList);
     }
 }
